79e5b408d5836e484167a28b8c43252225b3be22
3B032112 <rjoeyic@gmail.com>
2024-01-09 18:37:57 +0800

    Update Route (Split up)

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,14 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource("Products", ProductController::class)->only([
-    'index', 'show', 'store', 'update', 'destroy'
-]);
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 /*
   GET|HEAD        Products ........................................................................................................................................................................ Products.index › ProductController@index
